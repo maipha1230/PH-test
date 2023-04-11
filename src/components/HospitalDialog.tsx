6b70ab5262424be6c0c22dccc9801f28d4101623
Phathompong <maipha1230@gmail.com>
2023-04-11 11:16:04 +0700

    update hospital CRUD

---
+++ src/components/HospitalDialog.tsx
@@ -0,0 +1,168 @@
+import React, { useState, useEffect } from 'react'
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { TextField, Button, Box } from "@mui/material"
+import { HospitalModel } from '../models/Hospital.model';
+import * as yup from "yup";
+import { Formik } from "formik";
+import { axiosInstance } from '../axios/axiosInstance';
+import { successAlert } from '../sweetAlert/sweetAlert';
+
+
+
+const hospitalSchema = yup.object().shape({
+    hospital_code: yup.string().required("กรุณากรอกรหัสโรงพยาบาล"),
+    hospital_name_th: yup.string().required("กรุณากรอกชื่อไทยโรงพยาบาล"),
+    hospital_name_en: yup.string().required("กรุณากรอกชื่ออังกฤษรหัสโรงพยาบาล"),
+});
+
+type dialogInput = {
+    open: boolean,
+    handleDialogClose: () => void,
+    hospitalId: number | null
+}
+
+export default function HospitalDialog({ open, handleDialogClose, hospitalId = null }: dialogInput) {
+    const [initialHospitalForm, setInitialHospitalForm] = useState<HospitalModel>({
+        hospital_code: "",
+        hospital_name_th: "",
+        hospital_name_en: "",
+    })
+
+    useEffect(() => {
+        checkInputForm()
+    }, [open, handleDialogClose])
+
+    const closeModal = () => {
+        resetFormHospital()
+        handleDialogClose()
+    }
+
+    const resetFormHospital = () => {
+        setInitialHospitalForm({
+            hospital_code: "",
+            hospital_name_th: "",
+            hospital_name_en: "",
+        })
+    }
+
+    const checkInputForm = async () => {
+        if (hospitalId) {
+            const res = await axiosInstance.get(`/hospitals/get-hospital/${hospitalId}`)
+            if (res.status == 200) {
+                setInitialHospitalForm({
+                    hospital_code: res.data.hospital_code,
+                    hospital_name_th: res.data.hospital_name_th,
+                    hospital_name_en: res.data.hospital_name_en,
+                })
+            }
+        }
+    }
+
+    const onsubmitHospitalForm = async (values: HospitalModel) => {
+        if (hospitalId) {
+            const res = await axiosInstance.put(`/hospitals/update-hospital/${hospitalId}`, {
+                hospital_code: values.hospital_code,
+                hospital_name_th: values.hospital_name_th,
+                hospital_name_en: values.hospital_name_en,
+            })
+            if (res.status == 201) {
+                successAlert(res.data).then(() => {
+                    closeModal()
+                })
+            }
+        } else {
+            const res = await axiosInstance.post('/hospitals/create-hospital', {
+                hospital_code: values.hospital_code,
+                hospital_name_th: values.hospital_name_th,
+                hospital_name_en: values.hospital_name_en,
+            })
+            if (res.status == 201) {
+                successAlert(res.data).then(() => {
+                    closeModal()
+                })
+            }
+        }
+    }
+
+    return (
+        <Dialog open={open} onClose={closeModal} fullWidth >
+            <DialogTitle>{hospitalId ? "แก้ไขโรงพยาบาล" : "เพิ่มโรงพยาบาล"}</DialogTitle>
+            <DialogContent>
+                <Formik
+                    onSubmit={onsubmitHospitalForm}
+                    initialValues={initialHospitalForm}
+                    validationSchema={hospitalSchema}
+                    enableReinitialize>
+                    {({
+                        values,
+                        errors,
+                        touched,
+                        handleBlur,
+                        handleChange,
+                        handleSubmit,
+                        isValid,
+                        dirty
+                    }) => (
+                        <form onSubmit={handleSubmit} style={{ width: "100%", marginTop: "1rem" }}>
+                            <Box
+                                display={"flex"}
+                                flexDirection="column"
+                                gap={"1rem"}
+                                sx={{ width: "100%", }}
+                            >
+                                <TextField
+                                    fullWidth
+                                    variant="outlined"
+                                    type={"text"}
+                                    label="รหัสโรงพยาบาล"
+                                    onBlur={handleBlur}
+                                    onChange={handleChange}
+                                    value={values.hospital_code}
+                                    name="hospital_code"
+                                    error={!!touched.hospital_code && !!errors.hospital_code}
+                                    helperText={touched.hospital_code && errors.hospital_code}
+                                ></TextField>
+                                <TextField
+                                    fullWidth
+                                    variant="outlined"
+                                    type={"text"}
+                                    label="ชื่อโรงพยาบาล(ไทย)"
+                                    onBlur={handleBlur}
+                                    onChange={handleChange}
+                                    value={values.hospital_name_th}
+                                    name="hospital_name_th"
+                                    error={!!touched.hospital_name_th && !!errors.hospital_name_th}
+                                    helperText={touched.hospital_name_th && errors.hospital_name_th}
+                                ></TextField>
+                                <TextField
+                                    fullWidth
+                                    variant="outlined"
+                                    type={"text"}
+                                    label="ชื่อโรงพยาบาล(อังกฤษ)"
+                                    onBlur={handleBlur}
+                                    onChange={handleChange}
+                                    value={values.hospital_name_en}
+                                    name="hospital_name_en"
+                                    error={!!touched.hospital_name_en && !!errors.hospital_name_en}
+                                    helperText={touched.hospital_name_en && errors.hospital_name_en}
+                                ></TextField>
+                                <Box display={"flex"} flexDirection={"row"} justifyContent={"center"} alignItems={"center"} gap={2}>
+                                    <Button type="submit" color="success" variant="contained" disabled={!isValid || !dirty}>
+                                        บันทัก
+                                    </Button>
+                                    <Button color="inherit" variant="contained" onClick={closeModal}>
+                                        ยกเลิก
+                                    </Button>
+                                </Box>
+                            </Box>
+                        </form>
+                    )}
+                </Formik>
+            </DialogContent>
+        </Dialog>
+    )
+}
--- src/pages/Hospital.tsx
+++ src/pages/Hospital.tsx
@@ -1,28 +1,32 @@
-import { Box, Button, Divider, Typography, useTheme } from '@mui/material'
+import { Box, Button, Divider, Typography, useTheme, TextField } from '@mui/material'
 import { DataGrid, GridColDef, GridToolbar } from '@mui/x-data-grid';
 import React, { useEffect, useState } from 'react'
-
 import { axiosInstance } from "../axios/axiosInstance.js"
-import { HospitalModel } from '../models/HospitalModel.js';
-
+import { HospitalModel } from '../models/Hospital.model.js';
+import HospitalDialog from '../components/HospitalDialog.js';
+import { ensureRemoveHospital, successAlert } from '../sweetAlert/sweetAlert.js';
 
 export default function Hospital() {
   const theme = useTheme();
   const [hospitals, setHospitals] = useState<HospitalModel[] | null>(null)
-
+  const [dialogHospital, setDialogHospital] = useState<boolean>(false)
+  const [hostpitalId, setHostpitalId] = useState<number | null>(null)
 
   const column_data: GridColDef[] = [
-    { field: 'hospital_code', headerName: 'Code', flex: 1 },
-    { field: 'hospital_name_th', headerName: 'Name TH', flex: 1 },
-    { field: 'hospital_name_en', headerName: 'Name EN', flex: 1 },
+    { field: 'hospital_code', headerName: 'รหัสโรงพยาบาล',  width: 200 },
+    { field: 'hospital_name_th', headerName: 'ชื่อไทย', flex: 1 },
+    { field: 'hospital_name_en', headerName: 'ชื่ออังกฤษ', flex: 1 },
     {
-      field: 'created_at', headerName: 'Created', flex: 1, valueFormatter: (params) => new Date(params.value).toLocaleDateString()
+      field: 'created_at', headerName: 'เพิ่มเมื่อ', width: 200, valueFormatter: (params) => new Date(params.value).toLocaleDateString()
     },
     {
-      field: 'hospital_id', headerName: 'Details', flex: 1, renderCell: (row) => {
-        return <Box display={"flex"} flexDirection={"row"} gap={1} justifyContent={"center"} alignItems={"center"}>
-          <Button fullWidth color='info' variant='contained' onClick={() => onDetailClick(row.value)}>
-            Details
+      field: 'hospital_id', headerName: 'จัดการ', flex: 1, renderCell: (row) => {
+        return <Box display={"flex"} flexDirection={"row"} gap={1} justifyContent={"center"} alignItems={"center"} width={"50%"}>
+          <Button fullWidth color='warning' variant='contained' onClick={() => onDetailClick(row.value)}>
+            แก้ไข
+          </Button>
+          <Button fullWidth color='error' variant='contained' onClick={() => onDeleteClick(row.value)}>
+            ลบ
           </Button>
         </Box>
       }
@@ -31,17 +35,45 @@ export default function Hospital() {
 
   //use effect
   useEffect(() => {
+    getHospitals()
+  }, [dialogHospital])
+
+  const getHospitals = () => {
     axiosInstance.get("/hospitals/get-hospitals").then((res) => {
       if (res.status == 200) {
         setHospitals(res.data)
       }
     })
-  }, [])
-
+  }
 
   const onDetailClick = (id: number) => {
-    console.log(id);
+    setHostpitalId(id)
+    handleDialogOpen()
   }
+
+  const onDeleteClick = (id: number) => {
+    ensureRemoveHospital().then((check) => {
+      if (check.isConfirmed) {
+        axiosInstance.delete(`hospitals/remove-hospital/${id}`).then((res) => {
+          if (res.status == 200) {
+            successAlert(res.data).then(() => {
+              getHospitals();
+            })
+          }
+        })
+      }
+    })
+  }
+
+  const handleDialogOpen = () => {
+    setDialogHospital(true)
+  }
+
+  const handleDialogClose = () => {
+    setDialogHospital(false)
+    setHostpitalId(null)
+  }
+
 
   return (
     <Box display={"flex"} flexDirection={"column"} p={2}>
@@ -50,22 +82,25 @@ export default function Hospital() {
       </Typography>
       <Divider />
       <Box pt={1.5}>
-        <Button variant='contained' color='success'>
+        <Button variant='contained' color='success' onClick={handleDialogOpen}>
           เพิ่มโรงพยาบาล
         </Button>
       </Box>
-      <Box width={"100%"} height={"70vh"} mt={3} px={1.5}>
+      <Box width={"100%"} maxWidth={"100wh"} height={"70vh"} mt={3}>
         {hospitals && (
           <DataGrid
             rows={hospitals}
             columns={column_data}
             getRowId={(row: any) => row.hospital_id}
-            components={{ Toolbar: GridToolbar }} 
+            components={{ Toolbar: GridToolbar }}
             showCellVerticalBorder
             showColumnVerticalBorder
-            />
+          />
         )}
       </Box>
+      {/* dialog hospital */}
+      <HospitalDialog open={dialogHospital} handleDialogClose={handleDialogClose} hospitalId={hostpitalId} />
+
     </Box>
   )
 }
