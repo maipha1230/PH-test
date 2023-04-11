8834e083acff91e8abe8f7a57eaf6415de0ae353
Phathompong <maipha1230@gmail.com>
2023-04-11 11:17:10 +0700

    update axios

--- src/axios/axiosInstance.ts
+++ src/axios/axiosInstance.ts
@@ -1,30 +1,34 @@
 import axios from "axios";
+import { waringAlert } from "../sweetAlert/sweetAlert";
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3001/api'
-})
-
+  baseURL: "http://localhost:3001/api",
+});
 
 axiosInstance.interceptors.request.use(
-    config => {
-        const token =  localStorage.getItem('access-token')
-        if (token) {
-            config.headers['Authorization'] = `Bearer ${token}` 
-        }
-        return config;
-    },
-    error => {
-        Promise.reject(error)
+  (config) => {
+    const token = localStorage.getItem("access-token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
     }
-)
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
 
 axiosInstance.interceptors.response.use(
-    function (response) {
-        return response;
-    },
-    async function (error) {
-        if (error.response.status === 401) {
-            localStorage.removeItem("access-token")
-            // window.location.href = "/"
-        }
+  function (response) {
+    return response;
+  },
+  async function (error) {
+    if (error.response.status === 401) {
+      localStorage.removeItem("access-token");
+      waringAlert(error.response.data).then(() => {
+        window.location.href = "/";
+      });
+    } else if (error.response.status === 400) {
+      waringAlert(error.response.data);
     }
-)
+  }
+);
